c3454f628d9930ddd696b37c850266cf7f19e398
user <deviindah@student.untan.ac.id>
2021-02-18 21:32:17 +0700

    praktikum 02 membuat objek dan class

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('mahasiswa.php');
+
+$devi = new mahasiswa();
+$ega = new mahasiswa();
+$qiqi = new mahasiswa();
+
+$devi->nim ='h1101191057';
+$devi->nama ='devi indah wulandari';
+$devi->tanggal_lahir ="27 mei 2001";
+$devi->jenis_kelamin ='perempuan';
+
+$ega->nim ='h1101191051';
+$ega->nama ='Ega fahira anggriani';
+$ega->tanggal_lahir ="16 agustus 2001";
+$ega->jenis_kelamin ='perempuan';
+
+$qiqi->nim ='h1101191005';
+$qiqi->nama ='Rizky firdhani muthmainah';
+$qiqi->tanggal_lahir ="26 februari 2002";
+$qiqi->jenis_kelamin ='perempuan';
---
+++ mahasiswa.php
@@ -0,0 +1,18 @@
+<?php
+class mahasiswa
+{
+    public $nim;
+    public $nama;
+    public $tanggal_lahir;
+    public $jenis_kelamin;
+
+public function tampilkanAngkatan()
+{
+
+}
+
+public function tampilkanUmur()
+}
+
+}
+}
