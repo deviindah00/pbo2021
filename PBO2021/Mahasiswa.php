59c13a5a2f27a72500c5b30b117e5ba08868810e
user <deviindah@student.untan.ac.id>
2021-05-01 21:53:48 +0700

    pratikum 08 final static

--- PBO2021/Mahasiswa.php
+++ PBO2021/Mahasiswa.php
@@ -1,55 +1,71 @@
-<?php
-    require_once("User.php");
-    class Mahasiswa extends User
+<? php
+ Aplikasi namespace ;
+class  Mahasiswa  memperluas  Pengguna {
+// atribut
+    dilindungi  $ nama ;
+    dilindungi  $ nim ;
+    dilindungi  $ tanggal_lahir ;
+    dilindungi  $ jenis_kelamin ;
+    const  AKTIF = 1 ;
+    const  NON_AKTIF = 2 ;
+    public  statis  $ Status = Diri :: AKTIF ;
+    publik  statistik  $ sks = 3 ;
+    publik  statistik  $ bobot = 12 ;
+
+    function  __construct ( $ nama , $ nim , $ tl , $ jk ) {
+        $ ini -> nama = $ nama ;
+        $ ini -> nim = $ nim ;
+        $ this -> tanggal_lahir = $ tl ;
+        $ this -> jenis_kelamin = $ jk ;
+}
+     fungsi  publik tampilkanAngkatan () {
+        echo  "merupakan angkatan tahun 20" . substr ( $ this -> nim , 5 , - 4 ). "<br>" ;
+    }
+    public  function  tampilkanUmur () {
+        echo  date_diff ( date_create ( $ this -> tanggal_lahir ), date_create ( 'today' )) -> y ;
+    }
+     fungsi  publik tampilkanNama ( $ nama ) {
+        echo  $ this -> nama ;   
+    }
+    // penyetel
+    public  function  setNama ( $ nama ) {
+        $ ini -> nama = $ nama ;
+    }
+    public  function  setNim ( $ nim ) {
+        $ ini -> nim = $ nim ;
+    }
+    public  function  setTanggalLahir ( $ tanggal_lahir ) {
+        $ ini -> tanggal_lahir = $ tanggal_lahir ;
+    }
+    public  function  setJanisKelamin ( $ jenis_kelamin ) {
+        $ this -> jenis_kelamin = $ jenis_kelamin ;
+    }
+    // getter
+     fungsi  publik getNama () {
+        kembalikan  $ this -> nama ;
+    }
+     fungsi  publik getNim () {
+        kembalikan  $ this -> nim ;
+    }
+     fungsi  publik getTanggalLahir () {
+        kembalikan  $ this -> tanggal_lahir ;
+    }
+     fungsi  publik getJenisKelamin () {
+        return  $ this -> jenis_kelamin ;
+    }
+    // statistik 
+     fungsi statistik  publik bergerak () 
     {
-        protected $nim;
-        protected $nama;
-        protected $tanggal_lahir;
-        protected $jenis_kelamin;
-
-        function __construct($nim, $nama, $tgl, $jk){
-            $this->nim = $nim;
-            $this->nama = $nama;
-            $this->tanggal_lahir = $tgl;
-            $this->jenis_kelamin = $jk;
-        }
-
-        public function tampilkanAngkatan(){
-            echo "Merupakan angkatan tahun 20" . substr($this->nim, 5, 2);
-        }
-        public function tampilkanUmur(){
-
-            echo date_diff(date_create($this->tanggal_lahir), 
-                date_create("today"))->y;
-        }
-        public function tampilkanNama(){
-            echo $this->nama;
-        }
-
-    public function setNama($nama){
-        $this->nama=$nama;
+        echo  "agen solusi, bukan agen perubahan" ;
     }
-    public function setNim($nim){
-        $this->nim=$nim;
+     tuntaskan fungsi publik  akhir () 
+    {
+        self :: bergerak ();
+        echo  'memperbaiki menjadi lebih baik' ;
     }
-    public function setTanggalLahir($tanggal_lahir){
-        $this->tanggal_lahir=$tanggal_lahir;
-    }
-    public function setJanisKelamin($jenis_kelamin){
-        $this->jenis_kelamin=$jenis_kelamin;
-    }
-
-    public function getNama(){
-        return $this->nama;
-    }
-    public function getNim(){
-        return $this->nim;
-    }
-    public function getTanggalLahir(){
-        return $this->tanggal_lahir;
-    }
-    public function getJenisKelamin(){
-        return $this->jenis_kelamin;
+    // praktikum08
+    public  static  function  hitungSks ()
+    {
+        echo  "jumlah SKS nya adalah" . "<b>" . diri :: $ sks * diri :: $ bobot . "<b>" . "<br/>" ;
     }
 }
-?> 
--- PBO2021/test_stringmut.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'StringMutator.php';
-
-$kata = new StringMutator('Dian Prawira');
-
-$hasil = $kata->bold()->italic()->underscore()->getWord();
-
-echo $hasil;
