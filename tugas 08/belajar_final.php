017ac394d6169573b03c9fe50f0fb916f415c456
deviindah00 <78527119+deviindah00@users.noreply.github.com>
2021-05-01 21:36:47 +0700

    Delete tugas 08 directory

--- tugas 08/belajar_final.php
+++
@@ -1,9 +0,0 @@
-<? php
-
-require_once ( 'vendor / autoload.php' );
-menggunakan  App \ MahasiswaBaru ;
-
-$ budi = new  MahasiswaBaru ( 'H12313' , 'Budi Jak' , '12123123' , 'L' , '123' );
-
-$ budi -> tuntaskan ();
-?>
