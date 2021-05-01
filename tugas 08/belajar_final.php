147f7ba19b8dd2897cdc3412ede01c73356a03b0
user <deviindah@student.untan.ac.id>
2021-05-01 21:27:39 +0700

    pratikum 02 membuat objek dan class

---
+++ tugas 08/belajar_final.php
@@ -0,0 +1,9 @@
+<? php
+
+require_once ( 'vendor / autoload.php' );
+menggunakan  App \ MahasiswaBaru ;
+
+$ budi = new  MahasiswaBaru ( 'H12313' , 'Budi Jak' , '12123123' , 'L' , '123' );
+
+$ budi -> tuntaskan ();
+?>
